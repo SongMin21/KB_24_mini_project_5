0b19a5983f053acdf975b9f9ed04484e60ac2706
Minju Kang <brio0106@gmail.com>
2026-06-21 13:06:34 +0900

    [settings] controller, dto, service 폴더 및 파일 생성

---
+++ backend/Thinking/src/main/java/org/scoula/comment/service/CommentServiceImpl.java
@@ -0,0 +1,11 @@
+package org.scoula.comment.service;
+
+public class CommentServiceImpl {
+    // 강민주
+
+    // 복원준
+
+    // 이현서
+
+    // 이현주
+}
---
+++ backend/Thinking/src/main/java/org/scoula/thinking/dto/ThinkingDTO.java
@@ -0,0 +1,12 @@
+package org.scoula.thinking.dto;
+
+public class ThinkingDTO {
+
+    // 강민주
+
+    // 복원준
+
+    // 이현서
+
+    // 이현주
+}
