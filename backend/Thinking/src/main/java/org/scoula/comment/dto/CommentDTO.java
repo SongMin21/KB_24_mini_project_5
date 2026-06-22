748c449a3d48046b455e02456fc56ab8a16de36b
Minju Kang <brio0106@gmail.com>
2026-06-22 17:52:55 +0900

    [create-comment] create comment service 구현

--- backend/Thinking/src/main/java/org/scoula/comment/dto/CommentDTO.java
+++ backend/Thinking/src/main/java/org/scoula/comment/dto/CommentDTO.java
@@ -1,4 +1,9 @@
 package org.scoula.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public class CommentDTO {
+    public static JsonInclude builder() {
+        return null;
+    }
 }
--- backend/Thinking/src/main/java/org/scoula/comment/service/CommentServiceImpl.java
+++ backend/Thinking/src/main/java/org/scoula/comment/service/CommentServiceImpl.java
@@ -1,7 +1,24 @@
 package org.scoula.comment.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.comment.dto.CommentCreateDTO;
+import org.scoula.comment.mapper.CommentMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
 public class CommentServiceImpl implements CommentService{
+    final private CommentMapper mapper;
+
     // 강민주
+    @Override
+    public void create(CommentCreateDTO dto) {
+        log.info("create service");
+        // dto의 내용 vo로 변경하여 create 호출
+        mapper.create(dto.toVO());
+    }
 
     // 복원준
 
