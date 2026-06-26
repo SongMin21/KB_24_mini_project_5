566a1c0b0b1ccf4b3423a310f31553e6a17dd24f
Minju Kang <brio0106@gmail.com>
2026-06-26 14:40:10 +0900

    [create-comment] CommonDTO 파일 삭제

--- backend/Thinking/src/main/java/org/scoula/comment/dto/CommentDTO.java
+++
@@ -1,9 +0,0 @@
-package org.scoula.comment.dto;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-public class CommentDTO {
-    public static JsonInclude builder() {
-        return null;
-    }
-}
