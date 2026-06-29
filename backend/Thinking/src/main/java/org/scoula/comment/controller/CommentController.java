6d4e4555767cf86b104ae1eac0cf563ead5f6a5d
hyunseo-lee <hyunseo2503@gmail.com>
2026-06-29 18:02:05 +0900

    [update-comment] controller 구현중

--- backend/Thinking/src/main/java/org/scoula/comment/controller/CommentController.java
+++ backend/Thinking/src/main/java/org/scoula/comment/controller/CommentController.java
@@ -1,12 +1,34 @@
 package org.scoula.comment.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.scoula.comment.dto.CommentUpdateDTO;
+import org.scoula.comment.service.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comment")
+@RequiredArgsConstructor
+@Log4j2
 public class CommentController {
+
+    private final CommentService service;
 
     // 강민주
 
     // 복원준
 
     // 이현서
+    @PutMapping("/{id}")
+    public ResponseEntity<CommentUpdateDTO> updateComment(
+            @PathVariable Long id,
+            @RequestBody CommentUpdateDTO comment){
+        //
+        comment.setId(id);
+        CommentUpdateDTO dto = service.updateComment(comment);
+        return ResponseEntity.ok(dto);
+    }
 
     // 이현주
 }
