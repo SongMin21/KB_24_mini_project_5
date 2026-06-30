c4e465b536e41b2a6b2e3a8a3bb45c1486148131
mantalgap <wjbok02@naver.com>
2026-06-30 15:08:35 +0900

    [feature/update-thinking] 게시글 수정 Controller 1차 수정

--- backend/Thinking/src/main/java/org/scoula/thinking/controller/ThinkingController.java
+++ backend/Thinking/src/main/java/org/scoula/thinking/controller/ThinkingController.java
@@ -1,11 +1,37 @@
 package org.scoula.thinking.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.scoula.thinking.domain.ThinkingVO;
+import org.scoula.thinking.dto.ThinkingDTO;
+import org.scoula.thinking.dto.ThinkingUpdateDTO;
+import org.scoula.thinking.service.ThinkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/thinking")
+@RestController
+@Slf4j
+
 public class ThinkingController {
 
+    @Autowired
+    private ThinkingService service;
     // 강민주
 
     // 복원준
 
+    @PutMapping("")
+    public ResponseEntity<String> updateThinking(ThinkingUpdateDTO dto){
+        log.info("게시글 수정 요청" + dto);
+
+        //서비스 계층 로직 실행
+        if(service.updateThinking(dto)){
+            return ResponseEntity.ok("success");
+        } else {
+            return ResponseEntity.status(401).body("fail");
+        }
+    }
     // 이현서
 
     // 이현주
