a235806aada6af470a588ab928305b3a4cd8cf95
Minju Kang <brio0106@gmail.com>
2026-07-01 15:44:44 +0900

    [exception-aop] ExceptionAdvice 구현 수정

--- backend/Thinking/src/main/java/org/scoula/exception/ApiExceptionAdvice.java
+++ backend/Thinking/src/main/java/org/scoula/exception/ApiExceptionAdvice.java
@@ -1,19 +1,45 @@
 package org.scoula.exception;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(2)
 public class ApiExceptionAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .header("Content-Type", "text/plain;charset=UTF-8")
-                .body("해당 요소가 존재하지 않습니다.");
+                .body(e.getMessage());
     }
 
+    // 비밀번호 불일치 -> 401
+    @ExceptionHandler(PasswordMismatchException.class)
+    protected ResponseEntity<String> handlePasswordMismatchException(PasswordMismatchException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .header("Content-Type", "text/plain;charset=UTF-8")
+                .body(e.getMessage());
+    }
 
+    // 리소스 없음 -> 404
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .header("Content-Type", "text/plain;charset=UTF-8")
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .header("Content-Type", "text/plain;charset=UTF-8")
+                .body(e.getMessage());
+    }
 }
